8873ba17099170f61e15f0f26e26e2f4245adb3e
Henrik Oldenborg <henrik@oldenborg.nu>
2022-10-11 23:40:07 +0200

    Add poster styling

--- src/components/PosterTile.vue
+++ src/components/PosterTile.vue
@@ -7,6 +7,32 @@ defineProps<Props>();
 </script>
 
 <template>
-  <div>{{ title }}</div>
-  <img :src="image" />
+  <div class="poster">
+    <img class="image" :src="image" />
+    <div class="title">{{ title }}</div>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+@import "../assets/global.scss";
+
+.poster {
+  position: relative;
+  width: 100%;
+  max-width: 300px;
+
+  .title {
+    @extend .heading-font;
+    font-size: 2rem;
+    position: absolute;
+    bottom: 20px;
+    left: 20px;
+    text-overflow: wrap;
+  }
+
+  .image {
+    box-shadow: 0 0 10px 0px rgba(0, 0, 0, 0.3);
+    border: 10px solid white;
+  }
+}
+</style>
