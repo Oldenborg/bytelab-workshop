1b3c541acb5cfc7c2ce319452b8cd1ad8c70b9f2
Henrik Oldenborg <henrik@oldenborg.nu>
2022-10-12 02:05:38 +0200

    Add collection route

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,6 +8,11 @@ const router = createRouter({
       name: "home",
       component: () => import("@/views/HomeView.vue"),
     },
+    {
+      path: "/collection",
+      name: "collection",
+      component: () => import("@/views/CollectionView.vue"),
+    },
   ],
 });
 
